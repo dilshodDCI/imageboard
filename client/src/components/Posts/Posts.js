6cd716c935707f29fa1593171b183fa2c9c6fa66
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-04-23 11:34:58 +0200

    get all posts, style with material ui

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,19 +2,23 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles";
+import {Grid, CircularProgress} from "@material-ui/core";
 
 const Posts = () => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
   console.log(posts);
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (<CircularProgress />) : (
+    <Grid className={classes.mainContainer} container alignContent="stretch" spacing={3}>
+      {posts.map((post)=>(
+        <Grid key={post._id} item xs={12} md={6}>
+          <Post post={post}/>
+        </Grid>
+        
+      ))}
+    </Grid>
+     
+    
   );
 };
 
