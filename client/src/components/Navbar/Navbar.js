c8284e5d2f5115ec2ccbc56ca457865f164a13a4
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-05-14 12:07:00 +0200

    dotenv, auth- form layout

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { AppBar, Toolbar, Avatar, Button, Typography } from "@material-ui/core";
+import camera from "../../images/camera.png";
+import { Link } from "react-router-dom";
+
+import useStyles from "./styles";
+
+const Navbar = () => {
+  const classes = useStyles();
+  const user = null;
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Imageboard
+        </Typography>
+        <img className={classes.image} src={camera} alt="icon" height="60" />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.profile}
+              alt={user.result.name}
+              src={user.result.imageurl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="primary"
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 //general setup
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -11,13 +13,13 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://dilshodDCI:dilshodDCI123@cluster0.ldws3.mongodb.net/imageboard36?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://dilshodDCI:dilshodDCI123@cluster0.ldws3.mongodb.net/imageboard36?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
